79a59af93c0f658f08448b0b706f00ae3d8e6a8c
leandronunesdev <nunes.pessoal@gmail.com>
2022-08-10 14:47:10 -0300

    style: update not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
 
+import * as S from './styles';
+
 export const NotFound = () => {
   return (
-    <>
-      <div>NotFound</div>
-      <Link to='/login'>Login</Link>
-    </>
+    <S.NotFoundSection>
+      <img src='https://i.imgflip.com/2k02zl.jpg' alt='Wasted Barney' />
+      <h1>Ops!</h1>
+      <h2>404 - Page not found</h2>
+      <p>
+        The page you are looking for might have been removed, had its name
+        changed or is temporarily unavailable
+      </p>
+      <Link to='/login'>Got to Login</Link>
+    </S.NotFoundSection>
   );
 };
---
+++ src/pages/NotFound/styles.ts
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components';
+
+export const NotFoundSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: 32px auto;
+  align-items: center;
+  text-align: center;
+`;
